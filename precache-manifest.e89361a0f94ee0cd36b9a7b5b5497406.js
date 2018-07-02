ac457f45fac20b443236167e15cd05e3f0150ea5
nmanikiran <nmanikiran15@gmail.com>
2018-07-02 09:41:33 +0530

    updated dist

---
+++ precache-manifest.e89361a0f94ee0cd36b9a7b5b5497406.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfc25766d75d2d1bb99a",
+    "url": "/VueFeed/js/chunk-vendors.dfc25766.js"
+  },
+  {
+    "revision": "a41141b1637d231ca25a",
+    "url": "/VueFeed/js/app.a41141b1.js"
+  },
+  {
+    "revision": "2a0675283c95567a7d9bfb21d00b052d",
+    "url": "/VueFeed/index.html"
+  },
+  {
+    "revision": "dfc25766d75d2d1bb99a",
+    "url": "/VueFeed/css/chunk-vendors.f22a3629.css"
+  },
+  {
+    "revision": "a41141b1637d231ca25a",
+    "url": "/VueFeed/css/app.d383a0a2.css"
+  }
+];
