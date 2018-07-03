e1eb728ca40b10b954215c0173beccdd73bb271b
nmanikiran <nmanikiran15@gmail.com>
2018-07-03 09:45:40 +0530

    bug fix, mobile responsive

---
+++ precache-manifest.6cb4ccc114c1507431f02d46c99bc51a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afb0334a015ca06b3535",
+    "url": "/VueFeed/js/chunk-vendors.afb0334a.js"
+  },
+  {
+    "revision": "e567b9a9b88e30cf5e85",
+    "url": "/VueFeed/js/app.e567b9a9.js"
+  },
+  {
+    "revision": "f5c3a509785809f2fd35ea4b2b61c02c",
+    "url": "/VueFeed/index.html"
+  },
+  {
+    "revision": "afb0334a015ca06b3535",
+    "url": "/VueFeed/css/chunk-vendors.30e3f49e.css"
+  },
+  {
+    "revision": "e567b9a9b88e30cf5e85",
+    "url": "/VueFeed/css/app.55876a9e.css"
+  }
+];
