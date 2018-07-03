2d03ca4b589deb31d4afba42bccf8f1855ab736f
nmanikiran <nmanikiran15@gmail.com>
2018-07-03 23:17:48 +0530

    lastest build

---
+++ dist/precache-manifest.936c373d8f793da4817b81aaa03280e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "afb0334a015ca06b3535",
+    "url": "/VueFeed/js/chunk-vendors.afb0334a.js"
+  },
+  {
+    "revision": "aad2f44a4c3f87b7c232",
+    "url": "/VueFeed/js/app.aad2f44a.js"
+  },
+  {
+    "revision": "840636273ac18b922bebbff6de870047",
+    "url": "/VueFeed/index.html"
+  },
+  {
+    "revision": "830980e0f796d7e5f835d1bf7ee3a2d2",
+    "url": "/VueFeed/img/github-circle-white-transparent.830980e0.svg"
+  },
+  {
+    "revision": "afb0334a015ca06b3535",
+    "url": "/VueFeed/css/chunk-vendors.30e3f49e.css"
+  },
+  {
+    "revision": "aad2f44a4c3f87b7c232",
+    "url": "/VueFeed/css/app.aed2c6f4.css"
+  }
+];
